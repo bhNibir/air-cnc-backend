d1f0b23b2062c8f88e33a4393bda0b739286675d
nibir <biplob.asanibir@gmail.com>
2020-06-04 18:19:36 +0600

    add-home and getes all-homes route

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const app = express()
 const port = process.env.PORT || 4000
 const cors = require('cors')
 const bodyParser = require('body-parser')
+require('dotenv').config()
+
+//Database
+const MongoClient = require('mongodb').MongoClient;
+const uri =  process.env.DB_PATH
+let client = new MongoClient(uri, { useNewUrlParser: true });
 
 // Middleware
 app.use(cors())
@@ -12,13 +18,47 @@ app.get('/', (req, res) => {
     res.send("Hi")
 })
 
-app.get('/get-home', (req, res) => {
-    res.send({name: "Alif"})
-})
+app.get('/get-home', (req, res) =>  {
+
+        client = new MongoClient(uri, { useNewUrlParser: true });
+        client.connect(err => {
+        const collection = client.db("air-cnc").collection("homes");
+        collection.find().toArray((err, documents) => {
+            if(err){
+                console.log(err);
+                res.status(500).send({message:err})
+                
+            }
+            else{
+                res.send(documents)
+            }
+        })
+        client.close();
+        });
+    })
+
 
 app.post('/add-home', (req, res) => {
-    console.log(req.body)
-    res.send("Get Data")
+    const items = req.body
+
+    client = new MongoClient(uri, { useNewUrlParser: true });
+    client.connect(err => {
+    const collection = client.db("air-cnc").collection("homes");
+    collection.insert(items, (err, result) => {
+        if(err){
+            console.log(err);
+            res.status(500).send({message:err})
+            
+        }
+        else{
+            res.send(result.ops[0])
+        }
+    })
+    client.close();
+    });
 })
 
 app.listen(port , ()=> console.log(`Listen at http://localhost:${port}`))
+
+
+
